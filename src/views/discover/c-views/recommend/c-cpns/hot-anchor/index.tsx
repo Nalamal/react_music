f3beb06f5fe9a52894e85c2ef213f4333cb0a66a
贺煜林 <1981766322@qq.com>
2023-05-12 10:58:33 +0800

    feat(hot-anchor): complete hot-anchor development

--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { AnchorWrapper } from './style'
 import AreaHeaderV2 from '@/components/area-header-v2'
+import { hotRadios } from '@/assets/data/local_data'
 
 interface IProps {
   children?: ReactNode
@@ -10,7 +11,22 @@ interface IProps {
 const HotAnchor: FC<IProps> = () => {
   return (
     <AnchorWrapper>
-      <AreaHeaderV2></AreaHeaderV2>
+      <AreaHeaderV2 title="热门主播" />
+      <div className="anchors">
+        {hotRadios.map((item) => {
+          return (
+            <div className="item" key={item.picUrl}>
+              <a href="" className="image">
+                <img src={item.picUrl} alt="" />
+              </a>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="position">{item.position}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </AnchorWrapper>
   )
 }
