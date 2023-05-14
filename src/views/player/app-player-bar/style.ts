c731f784ebb98a4da8805d0a927bc1bf5edf3168
贺煜林 <1981766322@qq.com>
2023-05-14 11:38:02 +0800

    fix(player-style): fix progress style bug

--- src/views/player/app-player-bar/style.ts
+++ src/views/player/app-player-bar/style.ts
@@ -115,8 +115,12 @@ export const BarPlayInfo = styled.div`
           width: 22px;
           height: 24px;
           border: none;
-          margin-top: -7px;
+          margin-top: -4px;
           background: url(${require('@/assets/img/sprite_icon.png')}) 0 -250px;
+          &::before,
+          &::after {
+            display: none;
+          }
         }
       }
 
